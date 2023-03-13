e041b9a42c6242c4557e6c8e391110b5af686732
PascalUrso <pascal.urso@gmail.com>
2023-03-13 22:08:19 +0100

    typo

--- src/fr/uca/jgit/model/Node.java
+++ src/fr/uca/jgit/model/Node.java
@@ -1,7 +1,6 @@
 package fr.uca.jgit.model;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 public interface Node extends JGitObject {
     /** Restores the file node at the given path. **/
